f85100a960c79435a8dab91d9810b16f4d7ed145
Akshit Sharma <akshit.sharma@olacabs.com>
2022-06-28 22:11:13 +0530

    AS-0000 : Initial Commit

---
+++ AllClosedPullRequestsApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AllClosedPullRequestsApp
+//
+//  Created by Akshit Sharma on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    // MARK: Variables
+    
+    // MARK: LifeCycle Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+
+
+}
+
